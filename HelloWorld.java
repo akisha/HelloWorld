92475ea6977b2f4b4af1e46a81cf2fdba17a7507
akisha <akiliza@yandex.ru>
2016-09-04 21:22:54 +0300

    Hello World 1.0

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+/**
+ * Created by akisha on 04.09.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
